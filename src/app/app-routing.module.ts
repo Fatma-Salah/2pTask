4fbf1a58911843b1431e6aa10eaf5c312e9eb857
S-1-5-21-1124523052-1333741614-371417853-1002 <alexonfatma@gmail.com>
2024-04-20 05:58:31 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './view/Pages/user-list/user-list.component';
+import { UserDetailsComponent } from './view/Pages/user-details/user-details.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './view/includes/header/header.component';
+import { UserDetailsComponent } from './view/Pages/user-details/user-details.component';
+import { UserListComponent } from './view/Pages/user-list/user-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoadingComponent } from './view/includes/loading/loading.component';
+import { FooterComponent } from './view/includes/footer/footer.component';
+import { BannerComponent } from './view/includes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    LoadingComponent,
+    FooterComponent,
+    BannerComponent, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
